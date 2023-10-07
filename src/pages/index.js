112a4f3fd540769091048455e712a5b3df8c67d4
Ozmundooo <osman.ali.dev@gmail.com>
2023-10-06 20:36:12 -0400

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,195 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 export default function Home() {
+  const [start, setStart] = useState(0);
+
+  const [startingAmount, setStartingAmount] = useState(1);
+  const [thcPercent, setTHCPercent] = useState(1);
+  const [cbdPercent, setCBDPercent] = useState(2);
+
+  const [teaspoonsUsed, setTeaspoonsUsed] = useState(5);
+  const [totalServings, setTotalServings] = useState(10);
+  const [potencyResults, setPotencyResults] = useState({
+    totalTHC: 0,
+    totalCBD: 0,
+    totalTHCPerTeaspoon: 0,
+    totalCBDPerTeaspoon: 0,
+  });
+
+  // Function to calculate potency results
+  const calculatePotency = () => {
+    // Your potency calculation logic here
+    // This is just a placeholder for demonstration purposes
+    const totalTHC = startingAmount * thcPercent; // Replace with actual calculation
+    const totalCBD = startingAmount * cbdPercent; // Replace with actual calculation
+    const totalTHCPerTeaspoon = totalTHC / teaspoonsUsed; // Replace with actual calculation
+    const totalCBDPerTeaspoon = totalCBD / teaspoonsUsed; // Replace with actual calculation
+
+    // Return the calculated values
+    return {
+      totalTHC,
+      totalCBD,
+      totalTHCPerTeaspoon,
+      totalCBDPerTeaspoon,
+    };
+  };
+
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Call the calculatePotency function and update state with the results
+    const results = calculatePotency();
+    setPotencyResults(results);
+  };
+
+  useEffect(() => {
+    const results = calculatePotency();
+    setPotencyResults(results);
+    setTimeout(() => {
+      setStart(1);
+    }, 500);
+  }, []);
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="min-h-screen bg-secondary">
+      <h1
+        className={
+          start !== 0
+            ? "text-center pt-10 text-5xl font-title text-transparent tracking-tighter duration-1000 opacity-100 "
+            : "text-center pt-10 text-5xl font-title text-transparent  tracking-widest duration-1000 opacity-0"
+        }
+      >
+        Marijuana Mathematics
+      </h1>
+      <section className="">
+        <div>
+          <motion.form
+            initial={{ opacity: 0, y: 200 }}
+            animate={{ opacity: 100, y: 0 }}
+            transition={{ ease: "easeOut", duration: 1 }}
+            className="mx-6 my-5 bg-tertiary p-5 rounded-2xl font-subtitle"
+            onSubmit={handleSubmit}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <h2 className="font-title text-white text-2xl my-1">
+              Step 1: Calculate Potency For the Entire Batch
+            </h2>
+            <div className="flex flex-col my-2">
+              <label
+                className="font-subtitle text-white text-lg my-1"
+                htmlFor="startingAmount"
+              >
+                Starting Amount of Cannabis Product in Grams (g)
+              </label>
+              <input
+                type="number"
+                id="startingAmount"
+                className="py-2 pl-2 bg-transparent text-white border-b-2 focus:py-3 duration-500"
+                value={startingAmount}
+                onChange={(e) => setStartingAmount(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col my-2">
+              <label
+                htmlFor="thcPercent"
+                className="font-subtitle text-white text-lg my-1"
+              >
+                THC/THCA %
+              </label>
+              <input
+                type="number"
+                id="thcPercent"
+                className="py-2 pl-2 bg-transparent text-white border-b-2 focus:py-3 duration-500"
+                value={thcPercent}
+                onChange={(e) => setTHCPercent(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col my-2">
+              <label
+                className="font-subtitle text-white text-lg my-1"
+                htmlFor="cbdPercent"
+              >
+                CBD/CBDA %
+              </label>
+              <input
+                type="number"
+                className="py-2 pl-2 bg-transparent text-white border-b-2 focus:py-3 duration-500"
+                id="cbdPercent"
+                value={cbdPercent}
+                onChange={(e) => setCBDPercent(e.target.value)}
+              />
+            </div>
+            {/* 
+            <h2 className="font-title text-white text-2xl my-1">
+              Step 2: Creation Portions/Servings
+            </h2>
+            <div className="flex flex-col my-2">
+              <label
+                className="font-subtitle text-white text-lg my-1"
+                htmlFor="teaspoonsUsed"
+              >
+                Number of Teaspoons Used in Your Recipe
+              </label>
+              <input
+                type="number"
+                className="py-2 pl-2 bg-transparent text-white border-b-2 focus:py-3 duration-500"
+                id="teaspoonsUsed"
+                value={teaspoonsUsed}
+                onChange={(e) => setTeaspoonsUsed(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col my-2">
+              <label
+                className="font-subtitle text-white text-lg my-1"
+                htmlFor="totalServings"
+              >
+                Total Number of Servings In your Recipe
+              </label>
+              <input
+                type="number"
+                className="py-2 pl-2 bg-transparent text-white border-b-2 focus:py-3 duration-500"
+                id="totalServings"
+                value={totalServings}
+                onChange={(e) => setTotalServings(e.target.value)}
+              />
+            </div> */}
+
+            <button
+              className="my-3 border-white border-2 py-2 px-6 rounded-md text-white font-subtitle text-xl hover:bg-secondary hover:border-secondary duration-700"
+              type="submit"
+            >
+              Calculate
+            </button>
+          </motion.form>
+
+          <motion.div
+            initial={{ opacity: 0, y: 200 }}
+            animate={{ opacity: 100, y: 0 }}
+            transition={{ ease: "easeIn", duration: 1 }}
+            className="mx-6 my-5 bg-tertiary p-5 rounded-2xl font-subtitle "
+          >
+            <h2 className="font-title text-white text-2xl my-1">
+              Potency Results
+            </h2>
+            <p className="font-subtitle text-white text-lg my-1">
+              Total mg of THC: {potencyResults.totalTHC}
+              <span className="text-sm">mg</span>
+            </p>
+            <p className="font-subtitle text-white text-lg my-1">
+              Total mg of CBD: {potencyResults.totalCBD}
+              <span className="text-sm">mg</span>
+            </p>
+            <p className="font-subtitle text-white text-lg my-1">
+              Total mg of THC per teaspoon: {potencyResults.totalTHCPerTeaspoon}
+              <span className="text-sm">mg</span>
+            </p>
+            <p className="font-subtitle text-white text-lg my-1">
+              Total mg of CBD per teaspoon: {potencyResults.totalCBDPerTeaspoon}
+              <span className="text-sm">mg</span>
+            </p>
+          </motion.div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
-  )
+  );
 }
